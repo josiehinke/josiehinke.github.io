3b231a3bf88cc1a445471db88882c486de004e53
josiehinke <josieemma@gmail.com>
2016-08-12 15:40:04 -0700

    My first website

---
+++ events.js
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <title>My first HTML page</title>
+  <script type="text/javascript">
+  <!-- Hide
+   
+    //Your code will go here
+   
+  // End hide-->
+  </script>
+</head>
+<body>
+  <!-- Your page content will go here -->
+</body>
+</html>
+
